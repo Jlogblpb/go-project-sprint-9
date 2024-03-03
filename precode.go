bf280b1242362d175cdf21bd1241a832dbc22e1b
Jlogblbp <jlogblbp@yandex.ru>
2024-03-03 23:36:27 +0300

    Add 1,2,3 points

--- precode.go
+++ precode.go
@@ -5,29 +5,55 @@ import (
 	"fmt"
 	"log"
 	"sync"
+	"sync/atomic"
+	"time"
 )
 
-// --
 // Generator генерирует последовательность чисел 1,2,3 и т.д. и
 // отправляет их в канал ch. При этом после записи в канал для каждого числа
 // вызывается функция fn. Она служит для подсчёта количества и суммы
 // сгенерированных чисел.
 func Generator(ctx context.Context, ch chan<- int64, fn func(int64)) {
 	// 1. Функция Generator
-	// ...
+	var count atomic.Int64
+	count.Store(1)
+	for {
+		select {
+		case <-ctx.Done():
+			close(ch)
+			return
+		default:
+			count.Add(1)
+			ch <- count.Load()
+			fn(count.Load())
+		}
+	}
 }
 
 // Worker читает число из канала in и пишет его в канал out.
 func Worker(in <-chan int64, out chan<- int64) {
 	// 2. Функция Worker
-	// ...
+	for {
+		v, ok := <-in
+		if !ok {
+			//Закрыть канал и выйти
+			close(out)
+			return
+		}
+		//Пишем в канал out
+		out <- v
+		//пауза на 1 млсек
+		time.Sleep(1 * time.Millisecond)
+	}
 }
 
 func main() {
 	chIn := make(chan int64)
 
 	// 3. Создание контекста
-	// ...
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	// через 1 секунды вызываем cancel для отмены операции
+	defer cancel()
 
 	// для проверки будем считать количество и сумму отправленных чисел
 	var inputSum int64   // сумма сгенерированных чисел
@@ -56,14 +82,18 @@ func main() {
 	var wg sync.WaitGroup
 
 	// 4. Собираем числа из каналов outs
-	// ...
-
-	go func() {
-		// ждём завершения работы всех горутин для outs
-		wg.Wait()
-		// закрываем результирующий канал
-		close(chOut)
-	}()
+	for i := range outs {
+		wg.Add(1)
+		go func(in <-chan int64, i int64) {
+			amounts[i]++
+			chOut <- amounts[i]
+			defer wg.Done()
+			// ждём завершения работы всех горутин для outs
+			wg.Wait()
+			// закрываем результирующий канал
+			close(chOut)
+		}()
+	}
 
 	var count int64 // количество чисел результирующего канала
 	var sum int64   // сумма чисел результирующего канала
