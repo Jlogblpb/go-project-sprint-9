2315896b8ab3930a77ce1c7647ff953a295a8888
Jlogblbp <jlogblbp@yandex.ru>
2024-03-04 23:00:23 +0300

    Add atomic

--- precode.go
+++ precode.go
@@ -56,13 +56,13 @@ func main() {
 	defer cancel()
 
 	// для проверки будем считать количество и сумму отправленных чисел
-	var inputSum int64   // сумма сгенерированных чисел
-	var inputCount int64 // количество сгенерированных чисел
+	var inputSum atomic.Int64   // сумма сгенерированных чисел
+	var inputCount atomic.Int64 // количество сгенерированных чисел
 
 	// генерируем числа, считая параллельно их количество и сумму
 	go Generator(ctx, chIn, func(i int64) {
-		inputSum += i
-		inputCount++
+		inputSum.Add(i)
+		inputCount.Add(1)
 	})
 
 	const NumOut = 5 // количество обрабатывающих горутин и каналов
@@ -82,18 +82,6 @@ func main() {
 	var wg sync.WaitGroup
 
 	// 4. Собираем числа из каналов outs
-	// for v := range outs {
-	// 	wg.Add(1)
-	// 	go func(i int64) {
-	// 		defer wg.Done()
-	// 		amounts[i]++
-	// 		chOut <- i
-	// 		// ждём завершения работы всех горутин для outs
-	// 		wg.Wait()
-	// 		// закрываем результирующий канал
-	// 		close(chOut)
-	// 	}(v)
-	// }
 	var i int64
 	i = 0
 	for {
@@ -102,14 +90,14 @@ func main() {
 		go func(in <-chan int64, i int64) {
 			defer wg.Done()
 			amounts[i]++
-			chOut <- in
+			v := <-in
+			chOut <- v
 			// ждём завершения работы всех горутин для outs
 			wg.Wait()
 			// закрываем результирующий канал
 			close(chOut)
 		}(outs[i], i)
-
-	}
+	} //Переделать эту функцию на atomic
 
 	var count int64 // количество чисел результирующего канала
 	var sum int64   // сумма чисел результирующего канала
@@ -121,21 +109,21 @@ func main() {
 		sum = +v
 	}
 
-	fmt.Println("Количество чисел", inputCount, count)
-	fmt.Println("Сумма чисел", inputSum, sum)
+	fmt.Println("Количество чисел", inputCount.Load(), count)
+	fmt.Println("Сумма чисел", inputSum.Load(), sum)
 	fmt.Println("Разбивка по каналам", amounts)
 
 	// проверка результатов
-	if inputSum != sum {
-		log.Fatalf("Ошибка: суммы чисел не равны: %d != %d\n", inputSum, sum)
+	if inputSum.Load() != sum {
+		log.Fatalf("Ошибка: суммы чисел не равны: %d != %d\n", inputSum.Load(), sum)
 	}
-	if inputCount != count {
-		log.Fatalf("Ошибка: количество чисел не равно: %d != %d\n", inputCount, count)
+	if inputCount.Load() != count {
+		log.Fatalf("Ошибка: количество чисел не равно: %d != %d\n", inputCount.Load(), count)
 	}
 	for _, v := range amounts {
-		inputCount -= v
+		inputCount.Add(-1 * v)
 	}
-	if inputCount != 0 {
+	if inputCount.Load() != 0 {
 		log.Fatalf("Ошибка: разделение чисел по каналам неверное\n")
 	}
 }
